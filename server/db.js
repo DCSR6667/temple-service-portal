82ccf71b03edbc46cd341acebe89c1c7447b079f
DCSR6667 <sekharc446@gmail.com>
2025-08-24 15:26:24 -0400

    changed the url

--- server/db.js
+++ server/db.js
@@ -1,39 +1,30 @@
-const mongodb=require('mongodb');
-const MongoClient=mongodb.MongoClient;
-require('dotenv').config();
-const url=process.env.MONGO_URI;
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+require("dotenv").config();
+const url =
+  "mongodb+srv://chandra:chandra@cluster0.9ye9jop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 let _db;
 
-const initDB=callback=>
-{
-    if(_db)
-    {
-        return callback(null,_db);
-    }
+const initDB = (callback) => {
+  if (_db) {
+    return callback(null, _db);
+  }
 
-    MongoClient.connect(url)
-    .then(client=>
-    {
-        _db=client.db('vastra_seva');
-        callback(null,_db);
+  MongoClient.connect(url)
+    .then((client) => {
+      _db = client.db("vastra_seva");
+      callback(null, _db);
     })
-    .catch(err=>
-    {
-        callback(err);
-
-    })
+    .catch((err) => {
+      callback(err);
+    });
 };
 
-
-const getDB=()=>
-{
-    if(!_db)
-    {
-        throw Error("something went wrong");
-    }
-    return _db;
-
+const getDB = () => {
+  if (!_db) {
+    throw Error("something went wrong");
+  }
+  return _db;
 };
 
-
-module.exports={initDB,getDB};
+module.exports = { initDB, getDB };
